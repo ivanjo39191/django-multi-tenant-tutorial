269aa5a45d0c52f49a94462d990549ef29005190
ivanjo39191 <ivanjo39191@gmail.com>
2022-09-20 14:22:00 +0000

    feat: add admin

--- web/products/admin.py
+++ web/products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from products.models import Product, ProductCategory
 
 # Register your models here.
+admin.site.register(Product)         # 註冊 Product 模型
+admin.site.register(ProductCategory)    # 註冊 ProductCategory 模型
