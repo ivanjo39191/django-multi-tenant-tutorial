97404105bba12f85d87ca7fa6f75074874359966
ivanjo39191 <ivanjo39191@gmail.com>
2022-09-25 02:32:52 +0000

    feat: Day11 ModelAdmin，成為一名模型管理員

--- web/products/admin.py
+++ web/products/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from products.models import Product, ProductCategory
 
 # Register your models here.
-admin.site.register(Product)         # 註冊 Product 模型
-admin.site.register(ProductCategory)    # 註冊 ProductCategory 模型
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'category__name']
+    fields = ('name', 'description', 'price', 'category', 'created', 'modified')
+    list_display = ('name', 'price', 'category')
+    list_filter = ('category',)
+    autocomplete_fields = ['category']
+    readonly_fields = ('created', 'modified')
+
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    fields = ('name', 'description', 'created', 'modified')
+    list_display = ('name',)
+    readonly_fields = ('created', 'modified')
+
+
+admin.site.register(Product, ProductAdmin)         # 註冊 Product 模型
+admin.site.register(ProductCategory, ProductCategoryAdmin)    # 註冊 ProductCategory 模型
+
+
