216608ecba287276ede2f769c440cb2db7142b3a
ivanjo39191 <ivanjo39191@gmail.com>
2022-09-28 13:26:01 +0000

    feat: Day 16 佈置房間， 將資料傳入 Template

--- web/products/views.py
+++ web/products/views.py
@@ -1,13 +1,20 @@
+from django.db import connection
 from django.views.generic import TemplateView, ListView, DetailView
-from products.models import Product
+from products.models import Product, ProductCategory
 
 class HomeView(TemplateView):
     template_name = "products/home.html"
 
     def get(self, request, *args, **kwargs):
         products = Product.objects.all()
+        left_product_categories = ProductCategory.objects.all()[0:2]
+        right_product_categories = ProductCategory.objects.all()[2:3]
+        right_product_category = right_product_categories.first() if right_product_categories else None
         context = self.get_context_data(**kwargs)
         context['items'] = products
+        context['schema_name'] = connection.schema_name
+        context['left_product_categories'] = left_product_categories
+        context['right_product_category'] = right_product_category
         return self.render_to_response(context)
 
 
