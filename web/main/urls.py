f2fdc0effcfad6984bd0fb0467536ca40c9c91fb
ivanjo39191 <ivanjo39191@gmail.com>
2022-10-03 15:41:05 +0000

    fix: add i18n url

--- web/main/urls.py
+++ web/main/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('products/', include(('products.urls', 'products'), namespace='products')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
